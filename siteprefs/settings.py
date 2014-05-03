a47fbe0ab365402356d54718409c9a4110438175
Brian Schott <brian.schott@nimbisservices.com>
2014-05-02 23:33:30 -0400

    add SITEPREFS_ENABLED_COMMANDS setting

--- siteprefs/settings.py
+++ siteprefs/settings.py
@@ -6,3 +6,7 @@ EXPOSE_MODEL_TO_ADMIN = getattr(settings, 'SITEPREFS_EXPOSE_MODEL_TO_ADMIN', Fal
 
 # Module name used by siteprefs.toolbox.autodiscover_siteprefs() to find preferences in application packages.
 PREFS_MODULE_NAME = getattr(settings, 'SITEPREFS_MODULE_NAME', 'settings')
+
+# List of commands called by manage.py where autodiscover is enabled
+ENABLED_COMMANDS = getattr(settings, "SITEPREFS_ENABLED_COMMANDS",
+                           ['runserver', 'runserver_plus', 'run_gunicorn', 'celeryd'])
