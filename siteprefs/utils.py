51623e2618830e25e42d333a487571768f10a92b
Igor Starikov <idlesign@yandex.ru>
2014-05-09 08:10:09 -0700

    Changed PrefProxy.__str__() behaviour.

--- siteprefs/utils.py
+++ siteprefs/utils.py
@@ -88,6 +88,9 @@ class PrefProxy(object):
         return self.get_value()
 
     def __str__(self):
+        return '%s' % self.get_value()
+
+    def __repr__(self):
         return '%s = %s' % (self.name, self.get_value())
 
 
