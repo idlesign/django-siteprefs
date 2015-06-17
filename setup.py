7b4ccf978a7fb77d2cefb87d123b3dd3c2471372
idle sign <idlesign@yandex.ru>
2015-06-17 21:00:40 +0600

    setup.py is updated.

--- setup.py
+++ setup.py
@@ -9,14 +9,19 @@ f.close()
 setup(
     name='django-siteprefs',
     version='.'.join(map(str, VERSION)),
+    url='http://github.com/idlesign/django-siteprefs',
+
     description='Reusable app for Django introducing site preferences system',
     long_description=readme,
-    author="Igor 'idle sign' Starikov",
+    license='BSD 3-Clause License',
+
+    author='Igor `idle sign` Starikov',
     author_email='idlesign@yandex.ru',
-    url='http://github.com/idlesign/django-siteprefs',
+
     packages=['siteprefs'],
     include_package_data=True,
     zip_safe=False,
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -29,5 +34,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
