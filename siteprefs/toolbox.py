d9e1739618ce6ad9eb24af12c9f936860b127984
idle sign <idlesign@yandex.ru>
2016-03-01 21:34:15 +0600

    Fixed check in `autodiscover_siteprefs()`.

--- siteprefs/toolbox.py
+++ siteprefs/toolbox.py
@@ -98,7 +98,7 @@ def autodiscover_siteprefs(admin_site=None):
         'importlib',  # Since Django 1.9
     ]
     # Do not discover anything if called from manage.py (e.g. executing commands from cli).
-    if package in skip_packages or (len(sys.argv) > 1 and sys.argv[1] in MANAGE_SAFE_COMMANDS):
+    if package not in skip_packages or (len(sys.argv) > 1 and sys.argv[1] in MANAGE_SAFE_COMMANDS):
         import_prefs()
         Preference.read_prefs(get_prefs())
         register_admin_models(admin_site)
