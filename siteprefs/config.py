05ac61ff62e785450bf17749109e606e21d15676
idle sign <idlesign@yandex.ru>
2016-06-29 21:42:02 +0600

    Test run fix.

--- siteprefs/config.py
+++ siteprefs/config.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,5 +11,11 @@ class SiteprefsConfig(AppConfig):
     verbose_name = _('Site Preferences')
 
     def ready(self):
+
+        # Shouldn't try to autodiscover in test as
+        # DB probably won't be initialized in the moment.
+        if 'test' in sys.argv[0]:
+            return
+
         from .toolbox import autodiscover_siteprefs
         autodiscover_siteprefs()
