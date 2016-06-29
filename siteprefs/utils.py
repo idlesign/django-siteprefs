667ec774cb0204c10a73fcd6b87d1a97033acc61
Igor Starikov <idlesign@yandex.ru>
2016-06-29 19:36:51 +0600

    Fixed KeyError in import_prefs (see #10)

--- siteprefs/utils.py
+++ siteprefs/utils.py
@@ -225,14 +225,18 @@ def import_module(package, module_name):
 
 def import_prefs():
     """Imports preferences modules from packages (apps)."""
+    
+    # settings.py locals if autodiscover_siteprefs() is in urls.py
     settings_locals = get_frame_locals(3)
 
-    project_package = settings_locals['__package__']  # Expected project layout introduced in Django 1.4
-    if not project_package:
-        # Fallback to old layout.
-        project_package = os.path.split(os.path.dirname(settings_locals['__file__']))[-1]
+    if 'self' not in settings_locals:  # If not SiteprefsConfig.ready()
+        # Try to import project-wide prefs.
 
-    # Try to import project-wide prefs.
-    import_module(project_package, PREFS_MODULE_NAME)
+        project_package = settings_locals['__package__']  # Expected project layout introduced in Django 1.4
+        if not project_package:
+            # Fallback to old layout.
+            project_package = os.path.split(os.path.dirname(settings_locals['__file__']))[-1]
+
+        import_module(project_package, PREFS_MODULE_NAME)
 
     import_project_modules(PREFS_MODULE_NAME)
