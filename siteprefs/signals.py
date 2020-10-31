8076af33e2da7479382882e3714b9b8f2ded2e42
idle sign <idlesign@yandex.ru>
2020-10-31 09:20:09 +0700

    Fixed deprecation warning.

--- siteprefs/signals.py
+++ siteprefs/signals.py
@@ -1,5 +1,8 @@
 from django.dispatch import Signal
 
 
-prefs_save = Signal(providing_args=['app', 'updated_prefs'])
-"""Issued when dynamic preferences models are saved."""
+prefs_save = Signal()
+"""Issued when dynamic preferences models are saved.
+providing_args=['app', 'updated_prefs']
+
+"""
